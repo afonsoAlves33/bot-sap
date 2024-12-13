e011e8362d20bccac68be18a2c11f6803466ac63
afonsoAlves33 <afonso.alves161110@gmail.com>
2024-12-13 09:51:31 -0300

    added window verification

--- BUSCAR_BAB.py
+++ BUSCAR_BAB.py
@@ -2,78 +2,81 @@ import pyautogui
 import time
 from window_manager import WindowManager
 
+"""
+    O nome das janelas do windows está hard coded então é possível que
+    quebre caso eu mude o idioma do pc ou o sap atualiaze, etc.
+"""
+
 
 def save_excel_sheet(excel_sheet_name: str, variant: str, code: str = "mb51", saving_path: str = r"\\bosch.com\dfsrb\DfsBR\loc\Ca1\ED\mse\MOE1\Dados\Planejamento_Tecnico\19 - Dados_BI_online\03 - Base de dados\teste_bot"):
     """
     Saves a excel spreadsheet by following a default route on SAP
     """
-    # time.sleep(5)
-    # print(pyautogui.position())
-    # time.sleep(3)
+    time.sleep(5)
+    print(pyautogui.position())
+    time.sleep(3)
     pyautogui.click(x=339, y=741)
 
-    # Maximizando a tela
-    time.sleep(10)
-    # pyautogui.press("f10")
-    # time.sleep(0.5)
-    # pyautogui.press("down")
-    # time.sleep(0.5)
-    # pyautogui.press("down")
-    # time.sleep(0.5)
-    # pyautogui.press("down")
-    # time.sleep(0.5)
-    # pyautogui.press("down")
-    # time.sleep(0.5)
-    # pyautogui.press("down")
-    # time.sleep(0.5)
-    # pyautogui.press("enter")
-    # time.sleep(1)
+    time.sleep(5)
     WindowManager.focus_window_by_title("SAP Logon 800")
+    time.sleep(3)
     WindowManager.maximize_window_by_title("SAP Logon 800")
+    time.sleep(5)
     pyautogui.press("enter")
     time.sleep(1)
     pyautogui.press("enter")
 
-    # Abrir a MB51
     time.sleep(10)
-    # pyautogui.click(x=125, y=148)
-    # time.sleep(1)
+
+    WindowManager.focus_window_by_title("PS0(1)/011 SAP Easy Access")
     pyautogui.write(code)
     time.sleep(1)
     pyautogui.press("enter")
 
     # Chamar a variante
+    
     time.sleep(8)
+    WindowManager.focus_window_by_title("PS0(1)/011 Cost Centers: Actl/Target/Variance: Selection")
     pyautogui.hotkey("shift","f5")
     time.sleep(2)
+    WindowManager.focus_window_by_title("PS0(1)/011 Find Variant")
     pyautogui.write(variant)
     time.sleep(1)
     pyautogui.press("enter")
     time.sleep(1)
+    WindowManager.focus_window_by_title("PS0(1)/011 Cost Centers: Actl/Target/Variance: Selection")
     pyautogui.press("F8")
 
     # Selecionar como lista detalhada
+
     time.sleep(150)
     time.sleep(9)
+    WindowManager.focus_window_by_title("PS0(1)/011 Cost Centers: Actual/Target/Varianc")
     pyautogui.hotkey("shift","F7")
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Summation Levels")
     pyautogui.write("3")
     time.sleep(3)
     pyautogui.press("enter")
 
 
     # Clicando em crédito
+
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Cost Centers: Actual/Target/Varianc")
     pyautogui.doubleClick(x=427, y=503)
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Select Report")
     pyautogui.click(x=134, y=243)
     time.sleep(3)
     pyautogui.press("enter")
 
     time.sleep(1200)
 
+    WindowManager.focus_window_by_title("PS0(1)/011 Display Actual Cost Line Items for Cost Centers")
     pyautogui.hotkey("ctrl","F8")
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Change Layout")
     pyautogui.click(x=514, y=310)
     time.sleep(3)
     pyautogui.hotkey("ctrl","a")
@@ -84,11 +87,11 @@ def save_excel_sheet(excel_sheet_name: str, variant: str, code: str = "mb51", sa
     time.sleep(3)
     pyautogui.click(x=641, y=654)
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Display Actual Cost Line Items for Cost Centers")
     pyautogui.hotkey("ctrl","shift", "F7")
     time.sleep(3)
+    WindowManager.focus_window_by_title("Save As")
     pyautogui.click(x=163, y=423)
-    # time.sleep(3)
-    # pyautogui.press("enter")
     time.sleep(3)
 
     #Escrever o nome da planilha
@@ -103,23 +106,30 @@ def save_excel_sheet(excel_sheet_name: str, variant: str, code: str = "mb51", sa
     # pyautogui.press("enter")
     pyautogui.click(x=524, y=630)
     time.sleep(1)
+    WindowManager.focus_window_by_title("Confirm Save As")
     pyautogui.press("left")
     time.sleep(1)
     pyautogui.press("enter")
     time.sleep(1)
     pyautogui.press("enter")
+    time.sleep(5)
+
+    try: 
+        WindowManager.focus_window_by_title("SAP GUI Security")
+        pyautogui.click(x=311, y=430)
+        time.sleep(3)
+    except Exception as e:
+        print("Probably the window SAP GUI Security did not open")
     print("done")
-    time.sleep(9)
-    pyautogui.hotkey("alt","F4")
+
+    WindowManager.close_window_by_title(f"{excel_sheet_name} - Excel")
     time.sleep(2)
-    pyautogui.hotkey("alt","F4")
-    time.sleep(1.5)
-    pyautogui.click(x=233, y=444)
+    WindowManager.close_window_by_title("PS0(1)/011 Display Actual Cost Line Items for Cost Centers")
     time.sleep(2)
-    pyautogui.hotkey("alt","F4")
+    WindowManager.close_window_by_title("SAP Logon 800")
     print("Sap Closed")
 
 
 if __name__ == "__main__":
     save_excel_sheet(excel_sheet_name="EXPORT_BAB_2024_consolidado_PPC_BI.XLSX", variant="BAB2024", code="s_alr_87013625")
-    # s_alr_87013625    
+    # s_alr_87013625   
--- BUSCAR_FPG_PROD.py
+++ BUSCAR_FPG_PROD.py
@@ -1,6 +1,9 @@
 import pyautogui
 import time
 
+from window_manager import WindowManager
+
+
 
 def save_excel_sheet(excel_sheet_name: str, variant: str, code: str = "mb51", saving_path: str = r"\\bosch.com\dfsrb\DfsBR\loc\Ca1\ED\mse\MOE1\Dados\Planejamento_Tecnico\19 - Dados_BI_online\03 - Base de dados\teste_bot"):
     """
@@ -10,78 +13,86 @@ def save_excel_sheet(excel_sheet_name: str, variant: str, code: str = "mb51", sa
 
     # Maximizando a tela
     time.sleep(10)
-    pyautogui.press("f10")
-    time.sleep(0.5)
-    pyautogui.press("down")
-    time.sleep(0.5)
-    pyautogui.press("down")
-    time.sleep(0.5)
-    pyautogui.press("down")
-    time.sleep(0.5)
-    pyautogui.press("down")
-    time.sleep(0.5)
-    pyautogui.press("down")
-    time.sleep(0.5)
-    pyautogui.press("enter")
-    time.sleep(1)
+    time.sleep(5)
+    WindowManager.focus_window_by_title("SAP Logon 800")
+    time.sleep(3)
+    WindowManager.maximize_window_by_title("SAP Logon 800")
+    time.sleep(5)
     pyautogui.press("enter")
     time.sleep(1)
     pyautogui.press("enter")
 
-    # Abrir a MB51
     time.sleep(10)
-    # pyautogui.click(x=125, y=148)
-    # time.sleep(1)
+
+    WindowManager.focus_window_by_title("PS0(1)/011 SAP Easy Access")
     pyautogui.write(code)
     time.sleep(1)
     pyautogui.press("enter")
 
+
     # Chamar a variante
     time.sleep(8)
+    WindowManager.focus_window_by_title("PS0(1)/011 Material Document List")
     pyautogui.hotkey("shift","f5")
     time.sleep(2)
+    WindowManager.focus_window_by_title("PS0(1)/011 Find Variant")
     pyautogui.write(variant)
     time.sleep(1)
     pyautogui.press("enter")
     time.sleep(1)
+    WindowManager.focus_window_by_title("PS0(1)/011 Material Document List")
     pyautogui.press("F8")
 
     # Selecionar como lista detalhada
     time.sleep(15)
+    WindowManager.focus_window_by_title("PS0(1)/011 Material Document List")
     pyautogui.hotkey("ctrl","shift","F12")
 
     # Baixar a planilha
     time.sleep(3)
+    WindowManager.focus_window_by_title("PS0(1)/011 Material Document List")
     pyautogui.hotkey("shift","F4")
-    time.sleep(3)
-    pyautogui.press("enter")
-    time.sleep(9)
+    time.sleep(7)
+
     # pyautogui.hotkey('win', 'left')
     # time.sleep(1)
+    WindowManager.focus_window_by_title("Save As")
+    pyautogui.click(x=163, y=423)
+    time.sleep(7)
+
+    #Escrever o nome da planilha
     pyautogui.write(excel_sheet_name.upper())
-    time.sleep(1.5)
+    time.sleep(4)
     pyautogui.hotkey('ctrl', 'l')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.write(saving_path)
-    time.sleep(1)
+    time.sleep(4)
     pyautogui.press("enter")
     time.sleep(6)
-    pyautogui.press("enter")
+    # pyautogui.press("enter")
+    pyautogui.click(x=524, y=630)
     time.sleep(1)
+    WindowManager.focus_window_by_title("Confirm Save As")
     pyautogui.press("left")
     time.sleep(1)
     pyautogui.press("enter")
     time.sleep(1)
     pyautogui.press("enter")
+    time.sleep(8)
+
+    try: 
+        WindowManager.focus_window_by_title("SAP GUI Security")
+        pyautogui.click(x=311, y=430)
+        time.sleep(3)
+    except Exception as e:
+        print("Probably the window SAP GUI Security did not open")
     print("done")
-    time.sleep(9)
-    pyautogui.hotkey("alt","F4")
+
+    WindowManager.close_window_by_title(f"{excel_sheet_name} - Excel")
     time.sleep(2)
-    pyautogui.hotkey("alt","F4")
-    time.sleep(1.5)
-    pyautogui.click(x=233, y=444)
+    WindowManager.close_window_by_title("PS0(1)/011 Material Document List")
     time.sleep(2)
-    pyautogui.hotkey("alt","F4")
+    WindowManager.close_window_by_title("SAP Logon 800")
     print("Sap Closed")
 
 
